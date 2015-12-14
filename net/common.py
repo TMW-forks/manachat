e6f231acf4dadfa2d383894e72d3df11ec815148
Joseph Botosh <rumly111@gmail.com>
2015-12-14 21:21:52 +0200

    SocketWrapper is now subclass of asyncore.dispatcher_with_send

--- net/common.py
+++ net/common.py
@@ -8,30 +8,20 @@ netlog = logging.getLogger("ManaChat.Net")
 netlog.setLevel(logging.DEBUG)
 
 
-class SocketWrapper(asyncore.dispatcher):
+class SocketWrapper(asyncore.dispatcher_with_send):
     """
     socket wrapper with file-like read() and write() methods
     """
     def __init__(self, host=None, port=0, buffer_size=1500, protodef={}):
-        asyncore.dispatcher.__init__(self)
+        asyncore.dispatcher_with_send.__init__(self)
         self.buffer_size = buffer_size
-        self.read_buffer = self.write_buffer = ''
+        self.read_buffer = ''
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.protodef = protodef
         if protodef == {}:
             netlog.warning("protodef is empty")
         if host is not None:
             self.connect((host, port))
-
-    def writable(self):
-        return len(self.write_buffer) > 0
-
-    def readable(self):
-        return True
-
-    def handle_write(self):
-        sent = self.send(self.write_buffer)
-        self.write_buffer = self.write_buffer[sent:]
 
     def handle_read(self):
         try:
@@ -54,8 +44,7 @@ class SocketWrapper(asyncore.dispatcher):
                     break
             data = self.read_buffer[:n]
             self.read_buffer = self.read_buffer[n:]
-        # print 'data=', repr(data)
         return data
 
     def write(self, data):
-        self.write_buffer += data
+        self.send(data)

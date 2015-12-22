d63f9fe859e44a83de108046c8ddbe7406b21b32
Joseph Botosh <rumly111@gmail.com>
2015-12-22 05:21:32 +0200

    add commands /sit, /stand, /goto, /turn, /respawn

--- commands.py
+++ commands.py
@@ -1,4 +1,3 @@
-import re
 
 from net import mapserv
 
@@ -21,6 +20,26 @@ def send_party_message(msg):
     mapserv.cmsg_party_message(msg)
 
 
+def set_direction(dir_str):
+    d = {"down": 0, "left": 2, "up": 4, "right": 6}
+    dir_num = d.get(dir_str.lower(), 0)
+    mapserv.cmsg_player_change_dir(dir_num)
+
+
+def sit_or_stand(cmd):
+    a = {"/sit": 2, "/stand": 3}
+    action = a[cmd]
+    mapserv.cmsg_player_change_act(action)
+
+
+def set_destination(arg):
+    try:
+        x, y = map(int, arg.split())
+        mapserv.cmsg_player_change_dest(x, y, 0)
+    except ValueError:
+        pass
+
+
 def parse_player_name(line):
     line = line.lstrip()
     if len(line) < 2:
@@ -30,13 +49,13 @@ def parse_player_name(line):
         if end < 0:
             return line[1:], ""
         else:
-            return line[1:end+1], line[end+3:]
+            return line[1:end + 1], line[end + 3:]
     else:
         end = line.find(" ")
         if end < 0:
             return line, ""
         else:
-            return line[:end], line[end+1:]
+            return line[:end], line[end + 1:]
 
 
 def process_line(line):
@@ -49,7 +68,7 @@ def process_line(line):
             arg = ""
         else:
             cmd = line[:end]
-            arg = line[end+1:]
+            arg = line[end + 1:]
         if cmd in ("/w", "/whisper"):
             nick, message = parse_player_name(arg)
             if len(nick) > 0 and len(message) > 0:
@@ -57,6 +76,16 @@ def process_line(line):
         elif cmd in ("/p", "/party"):
             if len(arg) > 0:
                 send_party_message(arg)
+        elif cmd == "/respawn":
+            mapserv.cmsg_player_respawn()
+        elif cmd in ("/dir", "/direction", "/turn"):
+            if len(arg) > 0:
+                set_direction(arg)
+        elif cmd in ("/sit", "/stand"):
+            sit_or_stand(cmd)
+        elif cmd in ("/goto", "/nav", "/navigate", "/dest", "/destination"):
+            if len(arg) > 0:
+                set_destination(arg)
         else:
             pass
             # chatlog.warning("[warning] command {} not found".format(cmd))

3b4a2586c5afa5eab3df7ce6b5e81577faaccc06
Joseph Botosh <rumly111@gmail.com>
2016-01-20 19:38:33 +0200

    auto healing other players

--- plugins/battlebot.py
+++ plugins/battlebot.py
@@ -1,16 +1,16 @@
-import time
 import net.mapserv as mapserv
 import net.charserv as charserv
 import net.stats as stats
 from net.common import distance
 from net.inventory import get_item_index
 from utils import extends, Schedule
-# from loggers import debuglog
+from loggers import debuglog
 
 
 __all__ = [ 'PLUGIN', 'init', 'target_id',
             'hp_healing_ids', 'hp_heal_at', 'mp_healing_ids', 'mp_heal_at',
-            'auto_attack', 'auto_pickup', 'auto_heal' ]
+            'auto_attack', 'auto_pickup', 'auto_heal_self',
+            'auto_heal_others' ]
 
 
 PLUGIN = {
@@ -20,15 +20,24 @@ PLUGIN = {
 }
 
 target_id = 0
+
 hp_healing_ids = [ 535, 541 ]
 hp_heal_at = 0.3
-hp_last_healed = 0
-mp_healing_ids = []
+hp_is_healing = False
+hp_prev_value = 0
+
+mp_healing_ids = [ 826 ]
 mp_heal_at = 0.5
-mp_last_healed = 0
+mp_is_healing = False
+mp_prev_value = 0
+
+players_taken_damage = {}
+player_damage_heal = 300
+
 auto_pickup = True
 auto_attack = True
-auto_heal = True
+auto_heal_self = True
+auto_heal_others = True
 
 
 @extends('smsg_being_action')
@@ -38,9 +47,24 @@ def being_action(data):
 
     global target_id
     if data.type in (0, 10):
-        if target_id == 0 and data.dst_id == charserv.server.account:
+
+        if (auto_attack and target_id == 0 and
+                data.dst_id == charserv.server.account):
             target_id = data.src_id
             mapserv.cmsg_player_change_act(target_id, 7)
+
+        if (auto_heal_others and
+                data.dst_id != charserv.server.account and
+                data.dst_id in mapserv.beings_cache and
+                mapserv.beings_cache[data.dst_id].type == 'player'):
+
+            players_taken_damage[data.dst_id] = players_taken_damage.get(
+                data.dst_id, 0) + data.damage
+
+            if players_taken_damage[data.dst_id] >= player_damage_heal:
+                mapserv.cmsg_chat_message("#inma {}".format(
+                    mapserv.beings_cache[data.dst_id].name))
+                players_taken_damage[data.dst_id] = 0
 
 
 @extends('smsg_being_remove')
@@ -76,36 +100,55 @@ def flooritem_appears(data):
         Schedule(dist * 0.4, 0, mapserv.cmsg_item_pickup, data.id)
 
 
+@extends('smsg_player_status_change')
+def player_status_change(data):
+    global hp_is_healing
+    if data.id == charserv.server.account:
+        if data.effect == 256:
+            hp_is_healing = True
+        elif data.effect == 0:
+            hp_is_healing = False
+
+
 @extends('smsg_player_stat_update_x')
 def player_stat_update(data):
-    global hp_last_healed, mp_last_healed
-
-    if not auto_heal:
+    if not auto_heal_self:
         return
+
+    global hp_prev_value, mp_prev_value
 
     if data.type == stats.HP:
         max_hp = mapserv.player_stats.get(stats.MAX_HP, 0)
-        if data.stat_value < max_hp * hp_heal_at:
-            now = time.time()
-            if now > hp_last_healed + 2:
-                for item_id in hp_healing_ids:
-                    index = get_item_index(item_id)
-                    if index > 0:
-                        mapserv.cmsg_player_inventory_use(index, item_id)
-                        hp_last_healed = now
-                        break
+        if data.stat_value < max_hp * hp_heal_at and not hp_is_healing:
+            healing_found = False
+            for item_id in hp_healing_ids:
+                index = get_item_index(item_id)
+                if index > 0:
+                    healing_found = True
+                    debuglog.info("Consuming %d", item_id)
+                    mapserv.cmsg_player_inventory_use(index, item_id)
+                    break
+            if not healing_found:
+                debuglog.info("Low health, but no HP healing item found")
+
+        hp_prev_value = data.stat_value
 
     elif data.type == stats.MP:
         max_mp = mapserv.player_stats.get(stats.MAX_MP, 0)
-        if data.stat_value < max_mp * mp_heal_at:
-            now = time.time()
-            if now > mp_last_healed + 2:
-                for item_id in mp_healing_ids:
-                    index = get_item_index(item_id)
-                    if index > 0:
-                        mapserv.cmsg_player_inventory_use(index, item_id)
-                        mp_last_healed = now
-                        break
+        if data.stat_value < max_mp * mp_heal_at and not mp_is_healing:
+            healing_found = False
+            for item_id in mp_healing_ids:
+                index = get_item_index(item_id)
+                if index > 0:
+                    healing_found = True
+                    debuglog.info("Consuming %d", item_id)
+                    mapserv.cmsg_player_inventory_use(index, item_id)
+                    break
+
+            if not healing_found:
+                debuglog.info("Low mana, but no MP healing item found")
+
+        mp_prev_value = data.stat_value
 
 
 def init(config):

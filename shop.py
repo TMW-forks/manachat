d4dc6757d323c5e7545a36134891a24d45062341
Joseph Botosh <rumly111@gmail.com>
2016-03-30 21:43:07 +0300

    added shop launcher (grafted from 38dad97b893da93d436ad92fe7cfae05dee5c9a6)

---
+++ shop.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python2
+
+import asyncore
+import logging
+import sys
+from ConfigParser import ConfigParser
+
+try:
+    import construct
+    del construct
+except ImportError:
+    import os
+    sys.path.insert(0, os.path.join(os.getcwd(), "external"))
+
+import net
+import net.mapserv as mapserv
+import plugins
+from utils import extends
+from itemdb import load_itemdb
+from loggers import debuglog
+
+
+@extends('smsg_player_warp')
+def player_warp(data):
+    mapserv.cmsg_map_loaded()
+
+
+@extends('smsg_map_login_success')
+def map_login_success(data):
+    mapserv.cmsg_map_loaded()
+
+
+@extends('smsg_being_chat')
+def being_chat(data):
+    id_, message = data.id, data.message
+    nick = mapserv.beings_cache[id_].name
+    m = "{} : {}".format(nick, message)
+    debuglog.info(m)
+
+
+@extends('smsg_player_chat')
+def player_chat(data):
+    debuglog.info(data.message)
+
+
+@extends('smsg_whisper')
+def got_whisper(data):
+    nick, message = data.nick, data.message
+    m = "[{} ->] {}".format(nick, message)
+    debuglog.info(m)
+
+
+@extends('smsg_gm_chat')
+def gm_chat(data):
+    m = "[GM] {}".format(data.message)
+    debuglog.info(m)
+
+
+if __name__ == '__main__':
+    rootLogger = logging.getLogger('')
+    rootLogger.addHandler(logging.NullHandler())
+
+    dbgh = logging.StreamHandler(sys.stdout)
+    dbgh.setFormatter(logging.Formatter("[%(asctime)s] %(message)s",
+                                        datefmt="%Y-%m-%d %H:%M:%S"))
+    debuglog.addHandler(dbgh)
+    debuglog.setLevel(logging.INFO)
+
+    shoplog = logging.getLogger('ManaChat.Shop')
+    shoplog.addHandler(dbgh)
+
+    config = ConfigParser()
+    config.read('manachat.ini')
+
+    load_itemdb('itemdb.txt')
+    plugin_list = config.get('Core', 'plugins').split()
+    plugins.load_plugins(config, *plugin_list)
+
+    net.login(host=config.get('Server', 'host'),
+              port=config.getint('Server', 'port'),
+              username=config.get('Player', 'username'),
+              password=config.get('Player', 'password'),
+              charname=config.get('Player', 'charname'))
+
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        mapserv.cleanup()

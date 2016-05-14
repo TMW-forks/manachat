55633751703bb511f18f21fdd1ea2ea28ad857f6
Joseph Botosh <rumly111@gmail.com>
2016-05-14 16:15:09 +0300

    remove find_nearest_being from battlebot

--- plugins/battlebot.py
+++ plugins/battlebot.py
@@ -9,6 +9,7 @@ from net.common import distance
 from net.inventory import get_item_index
 from utils import extends
 from loggers import debuglog
+from actor import find_nearest_being
 
 
 __all__ = [ 'PLUGIN', 'init',
@@ -42,7 +43,7 @@ player_damage_heal = 300
 
 aa_monster_types = []
 
-auto_pickup = False
+auto_pickup = True
 auto_attack = False
 auto_heal_self = False
 auto_heal_others = False
@@ -146,33 +147,6 @@ def being_remove(data):
     if target is not None and target.id == data.id:
         target = None
         aa_next_time = time.time() + 5.0
-
-
-def find_nearest_being(name='', type='', ignored_ids=[], allowed_jobs=[]):
-
-    if mapserv.beings_cache is None:
-        return None
-
-    min_disance = 1000000
-    px = mapserv.player_pos['x']
-    py = mapserv.player_pos['y']
-    nearest = None
-
-    for b in mapserv.beings_cache.values():
-        if b.id in ignored_ids:
-            continue
-        if name and b.name != name:
-            continue
-        if type and b.type != type:
-            continue
-        if allowed_jobs and b.job not in allowed_jobs:
-            continue
-        dist = distance(px, py, b.x, b.y)
-        if dist < min_disance:
-            min_disance = dist
-            nearest = b
-
-    return nearest
 
 
 def battlebot_logic(ts):
